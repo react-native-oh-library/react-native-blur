73f5c991a8f305db48670e73bc9236a3723e3762
xwx1317588 <xuwenqian8@h-partners.com>
2024-03-18 17:29:39 +0800

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import BlurView from './components/BlurView.harmony';
-import VibrancyView from './components/VibrancyView.harmony';
+import type { View } from 'react-native';
+
+import BlurViewUntyped from './components/BlurView.harmony';
+import VibrancyViewUntyped from './components/VibrancyView.harmony';
 
 import type { BlurViewProps as BlurViewPropsIOS } from '@react-native-community/blur/src/components/BlurView.ios';
 import type { BlurViewProps as BlurViewPropsAndroid } from '@react-native-community/blur/src/components/BlurView.android';
@@ -10,8 +12,16 @@ type BlurViewProps =
   | BlurViewPropsIOS
   | BlurViewPropsAndroid
   | BlurViewPropsHarmony;
+
 type VibrancyViewProps = VibrancyViewPropsIOS;
 
+const BlurView = BlurViewUntyped as React.ForwardRefExoticComponent<
+  BlurViewProps & React.RefAttributes<View>
+>;
+
+const VibrancyView = VibrancyViewUntyped as unknown as React.ForwardRefExoticComponent<
+  VibrancyViewProps & React.RefAttributes<View>
+>;
 
 export { BlurView, VibrancyView };
 export type { BlurViewProps, VibrancyViewProps };
